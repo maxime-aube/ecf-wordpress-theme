47c441ca2e7ada4e7dde88258ea547bee0acef1f
Maxime Aube <maxime.aube@protonmail.com>
2021-09-27 23:06:06 +0200

    fix single etudiant oubli c/c

--- single-etudiant.php
+++ single-etudiant.php
@@ -11,31 +11,31 @@
         <div class="field-content"><?php the_field('film'); ?></div>
       </div>
       <div class="field">
-        <div class="field-title">Son film</div>
+        <div class="field-title">Sa série</div>
         <div class="field-content"><?php the_field('serie'); ?></div>
       </div>
       <div class="field">
-        <div class="field-title">Son film</div>
+        <div class="field-title">Son jeu vidéo</div>
         <div class="field-content"><?php the_field('jeu_video'); ?></div>
       </div>
       <div class="field">
-        <div class="field-title">Son film</div>
+        <div class="field-title">Son héros/héroïne</div>
         <div class="field-content"><?php the_field('heros'); ?></div>
       </div>
       <div class="field">
-        <div class="field-title">Son film</div>
+        <div class="field-title">Son livre</div>
         <div class="field-content"><?php the_field('livre'); ?></div>
       </div>
       <div class="field">
-        <div class="field-title">Son film</div>
+        <div class="field-title">Sa chanson/div>
         <div class="field-content"><?php the_field('chanson'); ?></div>
       </div>
       <div class="field">
-        <div class="field-title">Son film</div>
+        <div class="field-title">Son application</div>
         <div class="field-content"><?php the_field('application'); ?></div>
       </div>
       <div class="field">
-        <div class="field-title">Son film</div>
+        <div class="field-title">Son site web/div>
         <div class="field-content"><?php the_field('site_web'); ?></div>
       </div>
       <div class="field">
